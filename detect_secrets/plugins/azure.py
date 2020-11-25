721cd986471a26200b58cabe9ac309f938c2cebd
Rogier Reedijk <Rogier.Reedijk@wigo4it.nl>
2020-11-25 08:45:22 +0100

    Add seperator after keyword

--- detect_secrets/plugins/azure.py
+++ detect_secrets/plugins/azure.py
@@ -8,12 +8,12 @@ from detect_secrets.plugins.base import RegexBasedDetector
 
 class AzureDetector(RegexBasedDetector):
     """Scans for Azure keys."""
-    secret_type = 'Azure key'
+    secret_type = 'Azure key 3'
 
     denylist = [
-        re.compile(r'accountkey', re.IGNORECASE), #DefaultEndpointsProtocol=http;AccountName=account1;AccountKey=Abc1deF23gHIjkLmnOpQRStuVwxYZAB4CDeFG56hIJK7LMnoPq8RSTuVw9x0yz/A1BCDEFGhi/JKLMnopqRSTu==;
-        re.compile(r'sharedaccesskey', re.IGNORECASE), #Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AB1C2DeFGHIJ3KLmNOpQrst4uVWXYZaBCDefghijKlm=
-        re.compile(r'sharedaccesssignature', re.IGNORECASE), #BlobEndpoint=https://blobstoragewebsiteso.blob.core.windows.net/;SharedAccessSignature=sv=2019-12-12&ss=bfqt&srt=c&sp=rwdlacupx&se=2020-11-12T18:35:10Z&st=2020-11-12T10:35:10Z&spr=https&sig=Ab%1CDeFGHIjKlmNoPqRs2tUVwXYZAbcD3eFGhI4jk5lM%6N
-        re.compile(r'sig=', re.IGNORECASE), #SAS-token BlobStorage #example: sv=2019-12-12&ss=bfqt&srt=c&sp=rwdlacupx&se=2020-11-12T18:35:10Z&st=2020-11-12T10:35:10Z&spr=https&sig=Ab%1CDeFGHIjKlmNoPqRs2tUVwXYZAbcD3eFGhI4jk5lM%6N
-        re.compile(r'instrumentationkey', re.IGNORECASE), #InstrumentationKey=1234ab56-78cd-9e90-f1ab-12345c6f89af;IngestionEndpoint=https://westeurope-1.in.applicationinsights.azure.com/
+        re.compile(r'accountkey(?:\s|=|:|"|^)', re.IGNORECASE), #DefaultEndpointsProtocol=http;AccountName=account1;AccountKey=Abc1deF23gHIjkLmnOpQRStuVwxYZAB4CDeFG56hIJK7LMnoPq8RSTuVw9x0yz/A1BCDEFGhi/JKLMnopqRSTu==;
+        re.compile(r'sharedaccesskey(?:\s|=|:|"|^)', re.IGNORECASE), #Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AB1C2DeFGHIJ3KLmNOpQrst4uVWXYZaBCDefghijKlm=
+        re.compile(r'sharedaccesssignature(?:\s|=|:|"|^)', re.IGNORECASE), #BlobEndpoint=https://blobstoragewebsiteso.blob.core.windows.net/;SharedAccessSignature=sv=2019-12-12&ss=bfqt&srt=c&sp=rwdlacupx&se=2020-11-12T18:35:10Z&st=2020-11-12T10:35:10Z&spr=https&sig=Ab%1CDeFGHIjKlmNoPqRs2tUVwXYZAbcD3eFGhI4jk5lM%6N
+        re.compile(r'sig(?:\s|=|:|"|^)', re.IGNORECASE), #SAS-token BlobStorage #example: sv=2019-12-12&ss=bfqt&srt=c&sp=rwdlacupx&se=2020-11-12T18:35:10Z&st=2020-11-12T10:35:10Z&spr=https&sig=Ab%1CDeFGHIjKlmNoPqRs2tUVwXYZAbcD3eFGhI4jk5lM%6N
+        re.compile(r'instrumentationkey(?:\s|=|:|"|^)', re.IGNORECASE), #InstrumentationKey=1234ab56-78cd-9e90-f1ab-12345c6f89af;IngestionEndpoint=https://westeurope-1.in.applicationinsights.azure.com/
     ]
